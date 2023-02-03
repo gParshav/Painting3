023a2e84cbd77c92016025f8b287f5b166adb7e5
Parshav Gandhi <parshav.gandhi@tamasha.live>
2023-02-03 11:05:46 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+        <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
